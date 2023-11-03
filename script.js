f3f57a6c5c1753f7dd62a93d3b93464ac3ac2f3c
Oldrado Junior <oldradojunior@gmail.com>
2023-11-03 09:49:08 -0300

    fix: adjust mobile menu when opened

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
 document.querySelector('.menu').addEventListener('click', (e) => {
-  e.target.setAttribute('hidden', 'hidden');
-  document.querySelector('.close-menu').removeAttribute('hidden');
   document.querySelector('.menu-mobile').removeAttribute('hidden');
 });
 
 document.querySelector('.close-menu').addEventListener('click', (e) => {
-  e.target.setAttribute('hidden', 'hidden');
-  document.querySelector('.menu').removeAttribute('hidden');
   document.querySelector('.menu-mobile').setAttribute('hidden', 'hidden');
 });
